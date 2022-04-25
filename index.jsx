0e423e59564bb7c50582bfd8d84a68aa0acd28ba
sergeedouard <sjoseph.eng@gmail.com>
2022-04-25 10:55:05 -0400

    Upgrade Route system to use react-route-dom v6

--- index.jsx
+++ index.jsx
@@ -2,26 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { IntroPage } from "./javascript/pages/intro";
 import Home from "./javascript/pages/home";
-import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 export function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <IntroPage />
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<IntroPage />} />
+        <Route path="home" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const main = document.getElementById("main");
-  // const mainRoot = createRoot(main);
   ReactDOM.render(<App />, main);
-  // mainRoot.render(<App />);
 });
