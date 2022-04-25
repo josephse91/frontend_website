38224ec1a4750a91644ab5e19db871f6958c56e1
sergeedouard <sjoseph.eng@gmail.com>
2022-04-25 10:46:47 -0400

    Downgraded React Route dependencies

--- index.jsx
+++ index.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { IntroPage } from "./javascript/pages/intro";
+import Home from "./javascript/pages/home";
+import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+export function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <IntroPage />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const main = document.getElementById("main");
-  ReactDOM.render(<IntroPage />, main);
+  // const mainRoot = createRoot(main);
+  ReactDOM.render(<App />, main);
+  // mainRoot.render(<App />);
 });
